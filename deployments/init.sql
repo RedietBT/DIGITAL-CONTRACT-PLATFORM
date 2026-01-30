dafee7acf4e05a3d064dbef5b9bbf9f1663b1134
unknown <redietberhanu2@gmail.com>
2026-01-31 00:59:31 +0300

    docker set up done

---
+++ deployments/init.sql
@@ -0,0 +1,12 @@
+-- Create schemas for microservices isolation as per System Design
+CREATE SCHEMA IF NOT EXISTS auth_schema;
+CREATE SCHEMA IF NOT EXISTS profile_schema;
+CREATE SCHEMA IF NOT EXISTS contract_schema;
+CREATE SCHEMA IF NOT EXISTS signature_schema;
+CREATE SCHEMA IF NOT EXISTS document_schema;
+CREATE SCHEMA IF NOT EXISTS template_schema;
+CREATE SCHEMA IF NOT EXISTS wallet_schema;
+CREATE SCHEMA IF NOT EXISTS payment_schema;
+CREATE SCHEMA IF NOT EXISTS receipt_schema;
+CREATE SCHEMA IF NOT EXISTS analytics_schema;
+CREATE SCHEMA IF NOT EXISTS notification_schema;
