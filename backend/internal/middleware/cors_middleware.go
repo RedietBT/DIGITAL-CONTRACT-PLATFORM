1362a78baaa40d8460639375d276f1e2ed9ddd04
unknown <redietberhanu2@gmail.com>
2026-02-17 14:23:13 +0300

    coorecting the port and the uuid isssue on the contrat service

--- backend/internal/middleware/cors_middleware.go
+++ backend/internal/middleware/cors_middleware.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 // CORSMiddleware allows the Global Swagger UI to fetch the doc.json
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8085")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         // Added more headers just in case
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Token, x-access-token")
