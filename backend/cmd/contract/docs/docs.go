ec55cbf19c5fbe362a7458758ee6fb4a55446611
unknown <redietberhanu2@gmail.com>
2026-02-17 13:01:42 +0300

    coorecting the port and the uuid isssue on the contrat service

--- backend/cmd/contract/docs/docs.go
+++ backend/cmd/contract/docs/docs.go
@@ -1328,7 +1328,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8081",
+	Host:             "localhost:8083",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Digital Contract Platform - Contract Service",
