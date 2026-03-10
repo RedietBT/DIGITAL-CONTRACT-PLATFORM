4614a0cdd796003767d0f86d8803ea5401630c39
unknown <redietberhanu2@gmail.com>
2026-03-10 23:32:17 +0300

    signiture repo done correctly

---
+++ backend/internal/repository/signature_repo.go
@@ -0,0 +1,67 @@
+package repository
+
+import (
+	"context"
+	"crypto/sha256"
+	"fmt"
+
+	"github.com/RedietBT/DIGITAL-CONTRACT-PLATFORM/backend/internal/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type SignatureRepository interface {
+	CreateSignature(ctx context.Context, sig *models.Signature) error
+	GetSignatureByID(ctx context.Context, id uuid.UUID) (*models.Signature, error)
+	GetSignatureByContract(ctx context.Context, contractID uuid.UUID) ([]models.Signature, error)
+	DeleteSignature(ctx context.Context, id uuid.UUID, userID uuid.UUID) error
+}
+
+type signatureRepository struct {
+	db *gorm.DB
+}
+
+func NewSignatureRepository(db *gorm.DB) SignatureRepository {
+	return &signatureRepository{db: db}
+}
+
+func (r *signatureRepository) CreateSignature(ctx context.Context, sig *models.Signature) error {
+	// 1. Generate Hash of the VectorData for integrity
+	if len(sig.VectorData) > 0 {
+		hash := sha256.Sum256(sig.VectorData)
+		sig.Hash = fmt.Sprintf("%x", hash)
+	}
+
+	// 2. Persist to DB
+	return  r.db.WithContext(ctx).Create(sig).Error
+}
+
+// GetSignatureByID fetches a specific signature mark
+func (r *signatureRepository) GetSignatureByID(ctx context.Context, id uuid.UUID) (*models.Signature, error) {
+	var sig models.Signature
+	err := r.db.WithContext(ctx).First(&sig, "id = ?", id).Error
+	return &sig, err
+}
+
+
+// GetSignaturesByContract fetches all signatures for a document (to render them all)
+func (r *signatureRepository) GetSignatureByContract(ctx context.Context, contractID uuid.UUID) ([]models.Signature, error){
+	var signatures []models.Signature
+	err := r.db.WithContext(ctx).Where("contract_id = ?", contractID).Find(&signatures).Error
+	return signatures, err
+}
+
+// DeleteSignature allows a user to remove their mark BEFORE the contract is finalized
+func (r *signatureRepository) DeleteSignature(ctx context.Context, id uuid.UUID, userID uuid.UUID) error {
+	// We include userID to ensure only the owner can delete their own signature
+	result := r.db.WithContext(ctx).Where("id = ? AND user_id = ?", id, userID).Delete(&models.Signature{})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
+	return nil
+}
+
+
